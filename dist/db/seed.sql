61467a682e3f3ad31b1b6212acff9725335d7752
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-30 00:22:42 -0500

    update seed.sql

--- dist/db/seed.sql
+++ dist/db/seed.sql
@@ -1,5 +1,10 @@
 \c chat_db_dev;
 
+INSERT INTO users2 (user_email, user_name, user_password, manager, user_status)
+VALUES
+('admin@manager.com', 'admin', 'admin', true, 0),
+('hello@world.com', 'user1', 'password', false, 0);
+
 INSERT INTO rooms2 (room_name)
 VALUES
 ('Hello World'),
